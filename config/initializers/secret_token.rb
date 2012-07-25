336c47d0cdf731e5f66602e148ec4c376c72bd10
David Rice <me@davidjrice.co.uk>
2012-07-25 17:31:45 +0100

    p45 me

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P45::Application.config.secret_token = '813b7a4dd2d0628e853a6af7695944dd41ba1f201c32c8a68a0ff2b172cdb2b4d13362430c4a2d99c207c145f44a26e54a62b633cbcec013289879ddef54988b'
